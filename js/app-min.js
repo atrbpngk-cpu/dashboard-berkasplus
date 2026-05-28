1769c154681dbd1efa1eb5ce3c0bdd2caa5e6eb9
atrbpngk-cpu <atrbpngk@gmail.com>
2026-05-28 23:28:29 +0700

    Update app-min.js

--- js/app-min.js
+++ js/app-min.js
@@ -2311,6 +2311,7 @@ function lihatBerkasSeksi(namaSeksi) {
         <td class="border px-2 py-1">${row["Jenis permohonan"] || "-"}</td>
         <td class="border px-2 py-1">${row["Desa/Kecamatan"] || "-"}</td>
         <td class="border px-2 py-1">${row["Status Berkas"] || "-"}</td>
+        <td class="border px-2 py-1">${row["Nama petugas"] || "-"}</td>
         <td class="border px-2 py-1">${row["Keterangan"] || "-"}</td>
       </tr>
     `;
@@ -2332,6 +2333,7 @@ function downloadExcelSeksi() {
     "Jenis Permohonan": row["Jenis permohonan"],
     "Desa / Kecamatan": row["Desa/Kecamatan"],
     "Status": row["Status Berkas"],
+    "Nama petugas": row["Nama petugas"],
     "Posisi Terakhir": row["Posisi terakhir"],
     "Keterangan": row["Keterangan"]
   }));
